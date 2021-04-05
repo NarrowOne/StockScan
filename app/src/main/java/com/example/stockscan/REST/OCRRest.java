fac5920847de07ebd79fe01df61a80e793e3e0df
NarrowOne <comkilbane@gmail.com>
2021-04-05 16:02:03 +0100

    Refactored RestClient to abstract to reduce boilerplate in background thread while keeping ui thread adaptable

--- app/src/main/java/com/example/stockscan/REST/OCRRest.java
+++ app/src/main/java/com/example/stockscan/REST/OCRRest.java
@@ -1,148 +1,74 @@
 package com.example.stockscan.REST;
 
-import android.os.AsyncTask;
 import android.util.Log;
 
+import com.example.stockscan.Models.Produce;
 import com.example.stockscan.Models.ScanPopup;
 import com.google.gson.Gson;
-import com.google.gson.JsonObject;
 
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.io.BufferedReader;
-import java.io.DataOutputStream;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.net.HttpURLConnection;
-import java.net.URL;
-import java.util.HashMap;
 import java.util.Iterator;
 
-public class OCRRest extends AsyncTask<HashMap<String, Object>, String, String> {
-    private static final String TAG = "OCR REST Client - ";
-    String urlPath = "https://us-central1-stockscan-2d0f2.cloudfunctions.net/parse-image";
-    private ScanPopup popup;
-    private HashMap<String, Object> responseMap;
-    private int responseLineNum;
+public class OCRRest extends RestClient{
+    private static final String TAG = "OCR REST Client";
+    private final ScanPopup popup;
+    private final String reqMethod;
 
-
-    public OCRRest(ScanPopup popup){
+    public OCRRest(ScanPopup popup, String reqMethod){
+        super(reqMethod);
         this.popup = popup;
+        this.reqMethod = reqMethod;
     }
 
     @Override
     protected void onPreExecute() {
-        super.onPreExecute();
-        responseMap = new HashMap<>();
-        responseLineNum = 0;
-    }
-
-    @SafeVarargs
-    @Override
-    protected final String doInBackground(HashMap<String, Object>... hashMaps) {
-        HttpURLConnection connection;
-        OutputStreamWriter outWriter = null;
-        DataOutputStream outData;
-        BufferedReader inReader = null;
-        HashMap<String, Object> data = hashMaps[0];
-        StringBuilder response = new StringBuilder();
-        if(data.containsKey("image_data")) {
-            String imageData = (String) data.get("image_data");
-            String encodedData = "{\"image_data\" : \""+imageData+"\"}";
-
-            try {
-                URL url = new URL(urlPath);
-
-                connection = (HttpURLConnection) url.openConnection();
-                connection.setRequestMethod("POST");
-
-                connection.setRequestProperty("Content-Type", "application/json");
-                connection.setRequestProperty("Accept", "application/json");
-
-//                connection.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
-//                connection.setRequestProperty("Accept", "application/x-www-form-urlencoded");
-
-                connection.setDoOutput(true);
-                connection.setDoInput(true);
-                connection.setUseCaches(false);
-                connection.setAllowUserInteraction(false);
-
-                outData = new DataOutputStream(connection.getOutputStream());
-                outData.writeBytes(encodedData);
-                int responseCode = connection.getResponseCode();
-                inReader = new BufferedReader(new InputStreamReader(connection.getInputStream()), 8094);
-
-                String line;
-
-                while ((line = inReader.readLine()) != null) {
-                    response.append(line);
-                }
-
-                connection.disconnect();
-                inReader.close();
-
-            } catch (IOException e) {
-                e.printStackTrace();
-            } finally {
-                if (outWriter != null) {
-                    try {
-                        outWriter.close();
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-                }
-                if (inReader != null) {
-                    try {
-                        inReader.close();
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-                }
-            }
-        }else{
-            response.append("error");
-        }
-        return response.toString();
+        popup.show();
     }
 
     @Override
     protected void onProgressUpdate(String... values) {
-        super.onProgressUpdate(values);
-        responseMap.put(String.valueOf(responseLineNum), values[0]);
-        responseLineNum++;
+
     }
 
     @Override
     protected void onPostExecute(String s) {
-        super.onPostExecute(s);
-        Gson gson = new Gson();
-        JSONObject response = gson.fromJson(s, JSONObject.class);
+
+        if(s == null || s.matches("")) return;
+
         try {
+            JSONObject response = new JSONObject(s);
+
             if (!response.has("error")) {
+                if(response.has("produce_details")){
 
-                for (Iterator<String> it = response.keys(); it.hasNext(); ) {
-                    String key = it.next();
-                    Log.d(TAG, "{\"" + key + "\" : \"" + response.get(key) + "\"}");
+                    JSONObject prod = response.getJSONObject("produce_details");
+                    String produceDetails = prod.toString();
+                    popup.addProduce(new Gson().fromJson(produceDetails, Produce.class));
+
+                }else if(response.has("crud_result")){
+
+                    JSONObject result = response.getJSONObject("crud_result");
+                    if(result.getBoolean("error")){
+                        popup.showError();
+                    }else{
+                        popup.saved();
+                    }
+
                 }
-
             } else {
+                popup.showError();
                 Log.e(TAG, String.valueOf(response.get("error")));
+            }
+
+            JSONObject log = response.getJSONObject("Log");
+            for (Iterator<String> it = log.keys(); it.hasNext(); ) {
+                String key = it.next();
+                Log.i(TAG, "{\"" + key + "\" : \"" + log.get(key) + "\"}");
             }
         }catch (JSONException e) {
             e.printStackTrace();
         }
     }
-
-//    public void post(String encodedString, AsyncHttpResponseHandler responseHandler) {
-//        RequestParams params = new RequestParams();
-//        params.add("image_data", encodedString);
-////        post(url, params, responseHandler);
-//    }
-//
-//    public void post(String encodedString){
-//        String json = "{\"image_data\" : \""+encodedString+"\"}";
-//        post(url, "application/json", json);
-//    }
 }
--- app/src/main/java/com/example/stockscan/REST/RestClient.java
+++ app/src/main/java/com/example/stockscan/REST/RestClient.java
@@ -1,18 +1,124 @@
 package com.example.stockscan.REST;
 
-import com.loopj.android.http.AsyncHttpClient;
-import com.loopj.android.http.AsyncHttpResponseHandler;
-import com.loopj.android.http.RequestParams;
+import android.os.AsyncTask;
+import android.util.Log;
 
-public class RestClient {
-    private static final String TAG = "RestClient - ";
-    private static AsyncHttpClient client = new AsyncHttpClient();
+import com.example.stockscan.BuildConfig;
+import com.example.stockscan.Models.ScanPopup;
 
-    public static void get(String url, RequestParams params, AsyncHttpResponseHandler responseHandler){
-        client.get(url, params, responseHandler);
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.HashMap;
+import java.util.Iterator;
+
+public abstract class RestClient extends AsyncTask<HashMap<String, Object>, String, String> {
+    private static final String TAG = "REST Client";
+    String urlPath = "https://us-central1-stockscan-2d0f2.cloudfunctions.net/stockscan-test";
+    String reqMethod;
+
+    public RestClient(String reqMethod){
+        this.reqMethod = reqMethod;
     }
-    public static void post(String url, RequestParams params, AsyncHttpResponseHandler responseHandler){
-        client.post(url, params, responseHandler);
+
+
+    @SafeVarargs
+    @Override
+    protected final String doInBackground(HashMap<String, Object>... hashMaps) {
+        HttpURLConnection connection;
+        OutputStreamWriter outWriter = null;
+        DataOutputStream outData;
+        BufferedReader inReader = null;
+        HashMap<String, Object> data = hashMaps[0];
+        StringBuilder response = new StringBuilder();
+
+        String encodedData = getJson(data);
+
+
+        try {
+            URL url = new URL(urlPath);
+
+            connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod(reqMethod);
+
+            if (BuildConfig.DEBUG && !connection.getRequestMethod().equals(reqMethod)) {
+                throw new AssertionError("Assertion failed");
+            }
+            connection.setRequestProperty("Content-Type", "application/json");
+            connection.setRequestProperty("Accept", "application/json");
+
+//                connection.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
+//                connection.setRequestProperty("Accept", "application/x-www-form-urlencoded");
+
+            connection.setDoOutput(true);
+            connection.setDoInput(true);
+            connection.setUseCaches(false);
+            connection.setAllowUserInteraction(false);
+
+            outData = new DataOutputStream(connection.getOutputStream());
+            outData.writeBytes(encodedData);
+            int responseCode = connection.getResponseCode();
+            inReader = new BufferedReader(new InputStreamReader(connection.getInputStream()), 8094);
+
+            String line;
+
+            while ((line = inReader.readLine()) != null) {
+                response.append(line);
+            }
+
+            connection.disconnect();
+            inReader.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (outWriter != null) {
+                try {
+                    outWriter.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            if (inReader != null) {
+                try {
+                    inReader.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+        return response.toString();
     }
 
+    private String getJson(HashMap<String, Object> data) {
+        String json = "{\n";
+
+        int keyCount = 0;
+        for(String key : data.keySet()){
+            json += "\""+key+"\" : \""+data.get(key)+"\"";
+            if(keyCount != data.size()-1)
+                json += ",\n";
+            else
+                json += "\n}";
+
+            keyCount++;
+        }
+
+        return json;
+    }
+
+    @Override
+    protected abstract void onPreExecute();
+    @Override
+    protected abstract void onProgressUpdate(String... values);
+    @Override
+    protected abstract void onPostExecute(String s);
 }
