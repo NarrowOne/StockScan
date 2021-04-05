1197084548de7ce23da0584b3ae70b80525a3b8b
NarrowOne <comkilbane@gmail.com>
2021-04-05 15:59:21 +0100

    Removed unused code.

--- app/src/main/java/com/example/stockscan/Analyzers/ImageProcessorIMPL.java
+++ app/src/main/java/com/example/stockscan/Analyzers/ImageProcessorIMPL.java
@@ -1,16 +1,11 @@
 package com.example.stockscan.Analyzers;
 
-import android.app.ActivityManager;
-import android.content.Context;
 import android.graphics.ImageFormat;
 import android.graphics.Rect;
 import android.graphics.YuvImage;
 import android.media.Image;
 import android.os.Build;
-import android.os.SystemClock;
-import android.util.Log;
 
-import androidx.annotation.NonNull;
 import androidx.annotation.RequiresApi;
 import androidx.camera.core.ExperimentalGetImage;
 import androidx.camera.core.ImageProxy;
@@ -18,27 +13,18 @@ import androidx.camera.core.ImageProxy;
 import com.example.stockscan.Models.ScanPopup;
 import com.example.stockscan.REST.OCRRest;
 import com.example.stockscan.REST.RestClient;
-import com.example.stockscan.Utils.ScopedExecutor;
-import com.google.android.gms.tasks.Task;
-import com.google.android.gms.tasks.TaskExecutors;
-import com.google.mlkit.vision.common.InputImage;
 
 import java.io.ByteArrayOutputStream;
 import java.nio.ByteBuffer;
 import java.util.Base64;
 import java.util.HashMap;
 
-public abstract class ImageProcessorIMPL<T> implements ImageProcessor {
+public class ImageProcessorIMPL implements ImageProcessor {
+    private static final String TAG = "ImageProcessorBase";
     private boolean isShutdown;
     private boolean analysisRequested = false;
 
-    private final String TAG = "ImageProcessorBase";
-    private final ActivityManager activityManager;
-    private final ScopedExecutor executor;
-
-    protected ImageProcessorIMPL(Context context){
-        activityManager = (ActivityManager) context.getSystemService(Context.ACTIVITY_SERVICE);
-        executor = new ScopedExecutor(TaskExecutors.MAIN_THREAD);
+    public ImageProcessorIMPL(){
     }
 
     @Override
@@ -97,17 +83,6 @@ public abstract class ImageProcessorIMPL<T> implements ImageProcessor {
         return out.toByteArray();
     }
 
-
-
-    private Task<T> requestDetectInImage(final InputImage image, ScanPopup popup){
-        final long startMS = SystemClock.elapsedRealtime();
-
-        return detectInImage(image, popup).addOnFailureListener(executor, e -> {
-            String error = "Failed to process. Error: " + e.getLocalizedMessage();
-            Log.d(TAG, error);
-        });
-    }
-
     @Override
     public void stop() {
         isShutdown = true;
@@ -123,8 +98,5 @@ public abstract class ImageProcessorIMPL<T> implements ImageProcessor {
         analysisRequested = true;
     }
 
-    protected abstract Task<T> detectInImage(InputImage image, ScanPopup popup);
-    protected abstract void onSuccess(@NonNull T results);
-    protected abstract void onFailure(@NonNull Exception e);
 
 }
--- app/src/main/java/com/example/stockscan/Utils/JsonParser.java
+++
@@ -1,7 +0,0 @@
-package com.example.stockscan.Utils;
-
-public class JsonParser {
-    public static void decodeResponse(){
-
-    }
-}
